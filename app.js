b6043fdaaffb17f145286cd7ca2a63cec70f6ee7
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-07 16:03:47 +0700

    serving static file with expressJS

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
+
 // app.get("/", function (req, res) {
 //   res.send(`<h1>Hello World!</h1>`);
 // });
