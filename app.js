04adff68ffd91a29b6a09488a9a19337f94b987e
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-08 18:38:34 +0700

    using static file for styling and interactive

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static("public"));
 
 // app.get("/", function (req, res) {
-//   res.send(`<h1>Hello World!</h1>`);
+//    res.send(`<h1>Hello World!</h1>`);
 // });
 
 app.get("/restaurants", function (req, res) {
